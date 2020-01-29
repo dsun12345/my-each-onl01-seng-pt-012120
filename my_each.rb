036b3dacf8ead48721aae437d819f4f956721896
Daniel Sun <danielsun12345@gmail.com>
2020-01-29 13:32:22 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,10 @@
 def my_each(array)
-  if block_given?
+  
   i = 0 
   while i < array.length
-    yield (array[i])
+    yield array[i]
     i = i + 1 
   end
-  
-else
-  puts 
-  
+  array
 end
 
